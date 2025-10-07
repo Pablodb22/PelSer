f41da1bdbf8974c7f47febaf214beca6a38ad791
Pablo Durán <pabloduranballesteros@gmail.com>
2025-10-07 21:13:17 +0200

    arreglando errores registro 4

--- PelserNextjs/src/app/servicios/cliente.tsx
+++ PelserNextjs/src/app/servicios/cliente.tsx
@@ -1,8 +1,9 @@
 export async function registrarUsario(usuario: {nombre: string, apellidos: string, nombre_usuario: string, correo: string, contrasena: string,fecha_creacion: string}) {
-  const res = await fetch('/api/usuarios', {
-    method: 'POST',
-    body: JSON.stringify(usuario),
-    headers: { 'Content-Type': 'application/json' }
-  })
+const res = await fetch(`${window.location.origin}/api/usuarios`, {
+  method: 'POST',
+  body: JSON.stringify(usuario),
+  headers: { 'Content-Type': 'application/json' }
+})
+
   return res.json()
 }
