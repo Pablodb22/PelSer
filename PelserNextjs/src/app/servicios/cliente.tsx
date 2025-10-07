e8631bff57d0bc65bbc0477e7409750246713b2a
Pablo Durán <pabloduranballesteros@gmail.com>
2025-10-07 20:20:28 +0200

    Arreglando errores registro

--- PelserNextjs/src/app/servicios/cliente.tsx
+++ PelserNextjs/src/app/servicios/cliente.tsx
@@ -1,14 +1,8 @@
-import { NextResponse } from 'next/server'
-import { supabaseServer } from '../lib/supabaseServidor'
-
-export async function registrarUsario(usuario: {nombre: string, apellidos: string, nombre_usuario: string, correo: string, contrasena: string, fecha_creacion: string}) {
-
-    const { data, error } = await supabaseServer.from('usuarios').insert([usuario]).select()
-
-    if (error) {
-        console.log('Error al registrar el usuario:', error)
-        return NextResponse.json({ error: 'Error al registrar el usuario' }, { status: 500 })
-    }
-
-    return NextResponse.json({ ok: true, data }, { status: 200 })
+export async function registrarUsario(usuario: any) {
+  const res = await fetch('/api/usuarios', {
+    method: 'POST',
+    body: JSON.stringify(usuario),
+    headers: { 'Content-Type': 'application/json' }
+  })
+  return res.json()
 }
